16f5dfd8d03117738b2672740d90d211e48476a8
neshmasood <neshtam@gmail.com>
2022-04-06 20:34:46 -0500

    made two more models (assignments and families)

--- main_app/models.py
+++ main_app/models.py
@@ -6,31 +6,30 @@ from django.utils import timezone
 # Create your models here.
 
 class Transaction(models.Model):
-    transaction_number = models.CharField(max_length=50)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         # return self.transaction_number
-         return str(self.transaction_number) + ": $" + str(self.balance)
+         return ": $" + str(self.amount)
 
 STATUS_CHOICES = {
     ("Completed", "complete"), 
     ("Incomplete", "incomplete")
 }
 
-USER_CHOICES ={
-    ("child", "child"),
-    ("parent", "parent")
-}
+# USER_CHOICES ={
+#     ("child", "child"),
+#     ("parent", "parent")
+# }
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     duedate = models.DateTimeField()
-    status = models.CharField(max_length=10, choices = STATUS_CHOICES)
-
-    assigned_to = models.CharField(max_length=20, choices = USER_CHOICES)
-
+    description: models.CharField(max_length=200)
+    task_status = models.CharField(max_length=10, choices = STATUS_CHOICES)
+    task_approval = models.BooleanField()
+    assigned_to = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     transactions = models.ManyToManyField(Transaction) # M:M example
     created_at = models.DateTimeField(auto_now_add=True)
@@ -43,3 +42,16 @@ class Task(models.Model):
 
     class Meta: 
         ordering = ['name']
+
+
+
+class Families(models.Model):
+    name = models.CharField(max_length=50),
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Assignments(models.Model):
+    date = models.DateTimeField(default=timezone.now)   
